b32a3490bdd70c476b9979a324fbd34547d08b63
jirohas <naoki0110@gmail.com>
2025-12-02 18:42:18 +0900

    Add ms/Az variant for MAX mode UI

--- app.py
+++ app.py
@@ -664,6 +664,7 @@ with st.sidebar:
                     [
                         "熟考 + 鬼軍曹",
                         "熟考 (本気MAX)",
+                        "熟考 (本気MAX)ms/Az",
                         "熟考(メタ思考)+grok検索強化版",
                     ],
                     index=1  # デフォルト: 本気MAX
